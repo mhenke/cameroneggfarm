ae8cbd98d693be9fc42de38eedc6958d8ff8c4c5
Mike Henke <henke.mike@gmail.com>
2015-03-31 03:12:13 +0000

    update

--- public/dist/application.js
+++ public/dist/application.js
@@ -93,9 +93,10 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 
 
 angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+	function($scope, Authentication, $state) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+		$scope.$state = $state;
 	}
 ]);
 'use strict';
