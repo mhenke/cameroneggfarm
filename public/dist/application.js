3dcec468a3b21bbe80f424417abe2c8d764c5021
Mike Henke <henke.mike@gmail.com>
2015-04-01 17:19:26 +0000

    updated build

--- public/dist/application.js
+++ public/dist/application.js
@@ -44,6 +44,10 @@ angular.element(document).ready(function() {
 'use strict';
 
 // Use Applicaion configuration module to register a new module
+ApplicationConfiguration.registerModule('articles');
+'use strict';
+
+// Use Applicaion configuration module to register a new module
 ApplicationConfiguration.registerModule('core');
 'use strict';
 
@@ -54,6 +58,114 @@ ApplicationConfiguration.registerModule('main');
 
 // Use Applicaion configuration module to register a new module
 ApplicationConfiguration.registerModule('users');
+'use strict';
+
+// Configuring the Articles module
+angular.module('articles').run(['Menus',
+	function(Menus) {
+		// Set top bar menu items
+		Menus.addMenuItem('topbar', 'Articles', 'articles', 'dropdown', '/articles(/create)?');
+		Menus.addSubMenuItem('topbar', 'articles', 'List Articles', 'articles');
+		Menus.addSubMenuItem('topbar', 'articles', 'New Article', 'articles/create');
+	}
+]);
+'use strict';
+
+// Setting up route
+angular.module('articles').config(['$stateProvider',
+	function($stateProvider) {
+		// Articles state routing
+		$stateProvider.
+		state('listArticles', {
+			url: '/articles',
+			templateUrl: 'modules/articles/views/list-articles.client.view.html'
+		}).
+		state('createArticle', {
+			url: '/articles/create',
+			templateUrl: 'modules/articles/views/create-article.client.view.html'
+		}).
+		state('viewArticle', {
+			url: '/articles/:articleId',
+			templateUrl: 'modules/articles/views/view-article.client.view.html'
+		}).
+		state('editArticle', {
+			url: '/articles/:articleId/edit',
+			templateUrl: 'modules/articles/views/edit-article.client.view.html'
+		});
+	}
+]);
+'use strict';
+
+angular.module('articles').controller('ArticlesController', ['$scope', '$stateParams', '$location', 'Authentication', 'Articles',
+	function($scope, $stateParams, $location, Authentication, Articles) {
+		$scope.authentication = Authentication;
+
+		$scope.create = function() {
+			var article = new Articles({
+				title: this.title,
+				content: this.content
+			});
+			article.$save(function(response) {
+				$location.path('articles/' + response._id);
+
+				$scope.title = '';
+				$scope.content = '';
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		$scope.remove = function(article) {
+			if (article) {
+				article.$remove();
+
+				for (var i in $scope.articles) {
+					if ($scope.articles[i] === article) {
+						$scope.articles.splice(i, 1);
+					}
+				}
+			} else {
+				$scope.article.$remove(function() {
+					$location.path('articles');
+				});
+			}
+		};
+
+		$scope.update = function() {
+			var article = $scope.article;
+
+			article.$update(function() {
+				$location.path('articles/' + article._id);
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		$scope.find = function() {
+			$scope.articles = Articles.query();
+		};
+
+		$scope.findOne = function() {
+			$scope.article = Articles.get({
+				articleId: $stateParams.articleId
+			});
+		};
+	}
+]);
+'use strict';
+
+//Articles service used for communicating with the articles REST endpoints
+angular.module('articles').factory('Articles', ['$resource',
+	function($resource) {
+		return $resource('articles/:articleId', {
+			articleId: '@_id'
+		}, {
+			update: {
+				method: 'PUT'
+			}
+		});
+	}
+]);
 'use strict';
 
 // Setting up route
@@ -67,14 +179,15 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		state('home', {
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html',
-            controller: 'HomeController'
+            controller: 'HomeController',
+            pageTitle: 'Home'
 		});
 	}
 ]);
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus',
-	function($scope, Authentication, Menus) {
+angular.module('core').controller('HeaderController', ['$state', '$scope', 'Authentication', 'Menus',
+	function($state, $scope, Authentication, Menus) {
 		$scope.authentication = Authentication;
 		$scope.isCollapsed = false;
 		$scope.menu = Menus.getMenu('topbar');
@@ -82,6 +195,9 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 		$scope.toggleCollapsibleMenu = function() {
 			$scope.isCollapsed = !$scope.isCollapsed;
 		};
+		
+		$scope.state = $state;
+		$scope.pageTitle = $state.current.name;
 
 		// Collapsing the menu after navigation
 		$scope.$on('$stateChangeSuccess', function() {
@@ -273,23 +389,28 @@ angular.module('main').config(['$stateProvider',
 		$stateProvider.
 		state('privacy-policy', {
 			url: '/privacy-policy',
-			templateUrl: 'modules/main/views/privacy-policy.client.view.html'
+			templateUrl: 'modules/main/views/privacy-policy.client.view.html',
+            pageTitle: 'Privacy Policy'
 		}).
 		state('where-to-buy', {
 			url: '/where-to-buy',
-			templateUrl: 'modules/main/views/where-to-buy.client.view.html'
+			templateUrl: 'modules/main/views/where-to-buy.client.view.html',
+            pageTitle: 'Where To Buy'
 		}).
 		state('contacts', {
 			url: '/contacts',
-			templateUrl: 'modules/main/views/contacts.client.view.html'
+			templateUrl: 'modules/main/views/contacts.client.view.html',
+            pageTitle: 'Contacts'
 		}).
 		state('products', {
 			url: '/products',
-			templateUrl: 'modules/main/views/products.client.view.html'
+			templateUrl: 'modules/main/views/products.client.view.html',
+            pageTitle: 'Products'
 		}).
 		state('about-us', {
 			url: '/about-us',
-			templateUrl: 'modules/main/views/about-us.client.view.html'
+			templateUrl: 'modules/main/views/about-us.client.view.html',
+            pageTitle: 'About Us'
 		})
 	}
 ]);
