ddb5ad8769c7cfedf1eb664bbb76de9b31c06dfc
Mike Henke <henke.mike@gmail.com>
2015-04-12 21:18:47 +0000

    clean up some footer stuff

--- config/env/all.js
+++ config/env/all.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	app: {
-		title: 'cameroneggfarm',
+		title: 'cameronranch',
 		description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
 		keywords: 'MongoDB, Express, AngularJS, Node.js'
 	},
